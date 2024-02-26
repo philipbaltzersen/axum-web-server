0c4e3d7c549687f1796457af48444cd55ced37bf
Philip Holm <philipbaltzersen@gmail.com>
2024-02-26 08:02:46 +0100

    Add routing and handlers

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,50 @@
-use axum::{routing::get, Router};
+use axum::{
+    body::Body,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    routing::{get, post}, 
+    Json, Router,
+};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    name: String,
+    email: String,
+}
+
+// Handler for /create-user
+async fn create_user() -> impl IntoResponse {
+    Response::builder()
+        .status(StatusCode::CREATED)
+        .body(Body::from("User created"))
+        .unwrap()
+    }
+
+// Handler for /users
+async fn list_users() -> Json<Vec<User>> {
+    let users = vec![
+        User {
+            id: 1,
+            name: "Philip".to_string(),
+            email: "philip@email.com".to_string(),
+        },
+        User {
+            id: 2,
+            name: "Per".to_string(),
+            email: "per@email.com".to_string(),
+        },
+    ];
+    Json(users)
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello" }));
+    let app = Router::new()
+        .route("/", get(|| async { "Hello" }))
+        .route("/create-user", post(create_user))
+        .route("/users", get(list_users));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
